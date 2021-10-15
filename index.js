d52a64419d33aa68d9f2eebb312ba85233875b30
hana fleur <yukay2184@gmail.com>
2021-10-15 11:46:38 +0900

    新しいアンケートの実装

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const server = http
               secondItem: 'パン'
             })
           );
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: '寿司',
+              secondItem: 'ピザ'
+            })
+          )
         }
         res.end();
         break;
@@ -37,10 +45,9 @@ const server = http
             rawData = rawData + chunk;
           })
           .on('end', () => {
-            const qs = require('querystring');
-            const answer = qs.parse(rawData);
-            const body = answer['name'] + 'さんは' +
-              answer['favorite'] + 'に投票しました';
+            const answer = new URLSearchParams(rawData);
+            const body = answer.get('name') + 'さんは' +
+              answer.get('favorite') + 'に投票しました';
             console.info('[' + now + '] ' + body);
             res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
               body + '</h1></body></html>');
