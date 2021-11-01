569b42d1654fc3800517603bb3a4c96236240dbb
allow1119 <shuntarou_21n1101633@nnn.ed.jp>
2021-11-01 16:59:31 +0900

    完了

--- index.js
+++ index.js
@@ -11,20 +11,12 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        if (req.url === '/enquetes/yaki-shabu') {
+        if (req.url === '/enquetes/sushi-pizza') {
           res.write(
             pug.renderFile('./form.pug', {
               path: req.url,
-              firstItem: '焼き肉',
-              secondItem: 'しゃぶしゃぶ'
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
+              firstItem: '寿司',
+              secondItem: 'ピザ'
             })
           );
         }
