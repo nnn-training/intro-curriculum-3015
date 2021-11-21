ae6ba698f0b0c7491196305af3f77f3bccf949d8
Futa Yamato <fuuta_21n1200805@nnn.ed.jp>
2021-11-21 21:46:00 +0900

    アンケートを追加

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ const server = http
             })
           );
         }
+        else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: 'すし',
+              secondItem: 'ピザ'
+            })
+          );
+        }
         res.end();
         break;
       case 'POST':
