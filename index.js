a4a1961b085bb1a5919af77c7a62756c8a9cd77b
mkawada <m@llll.jp>
2022-01-20 11:20:34 +0900

    冷めたピザ vs 超高級寿司

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const server = http
               secondItem: 'パン'
             })
           );
+        } else if (req.url === '/enquetes/sushi-pizza') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: '超高級寿司',
+              secondItem: '冷めたピザ'
+            })
+          );
         }
         res.end();
         break;
