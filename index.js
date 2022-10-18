7737b19b047d111a15829dddfae4362e927835f5
DDRAGON <davide.0829@gmail.com>
2022-10-18 20:27:45 +0900

    寿司とピザのアンケートを追加

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const server = http
               secondItem: 'パン'
             })
           );
+        } else if (req.url === '/enquetes/sushi-piza') {
+          res.write(
+            pug.renderFile('./form.pug', {
+              path: req.url,
+              firstItem: '寿司',
+              secondItem: 'ピザ'
+            })
+          );
         }
         res.end();
         break;
