373915608d84d948267c34064b061d0a6503451a
ueta-shunsuke <206701210+chazuke-ueta@users.noreply.github.com>
2026-08-02 13:27:09 +0900

    練習

--- index.js
+++ index.js
@@ -11,22 +11,36 @@ const server = http
 
     switch (req.method) {
       case 'GET':
-        if (req.url === '/enquetes/yaki-tofu') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: '焼き肉',
-              secondItem: '湯豆腐'
-            })
-          );
-        } else if (req.url === '/enquetes/rice-bread') {
-          res.write(
-            pug.renderFile('./form.pug', {
-              path: req.url,
-              firstItem: 'ごはん',
-              secondItem: 'パン'
-            })
-          );
+        switch (req.url) {
+          case '/enquetes/yaki-tofu':
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: '焼き肉',
+                secondItem: '湯豆腐'
+              })
+            );
+            break;
+          case '/enquetes/rice-bread':
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: 'ごはん',
+                secondItem: 'パン'
+              })
+            );
+            break;
+          case '/enquetes/sushi-pizza':
+            res.write(
+              pug.renderFile('./form.pug', {
+                path: req.url,
+                firstItem: '寿司',
+                secondItem: 'ピザ'
+              })
+            );
+            break;
+          default:
+            break;
         }
         res.end();
         break;
